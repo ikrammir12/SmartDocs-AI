88c3f69757626ac1985fb19fe85d175d83da82f1
ikrammir12 <rahisaniikram@gmail.com>
2026-05-26 23:30:53 +0500

    Adding the main.py and setting up the Extraing Data from PDF file

---
+++ main.py
@@ -0,0 +1,29 @@
+from unstructured.partition.pdf import partition_pdf
+
+output_path = './content/'
+file_path = output_path + 'attention.pdf'
+
+#Extracting Data From PDF file
+
+def partition_pdf_file(file_path):
+    '''Partition a PDF file and return the elements.'''
+    chunks = partition_pdf(
+        filename=file_path,
+        infer_table_structure=True,
+        strategy='hi_res',
+        extract_image_block_types=['Image'],
+        extract_image_block_to_payload=True,
+        chunking_strategy='by_title',
+        max_characters=2000,
+        combine_text_under_n_characters=500,
+        new_after_n_characters=6000,
+
+    )
+    return chunks
+
+chunks = partition_pdf_file(file_path)
+
+
+
+
+
